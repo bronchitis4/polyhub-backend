56fbafcfeab1e43efea795e57138b809ecbce177
bronchitis4 <79788949+bronchitis4@users.noreply.github.com>
2025-06-05 00:10:50 +0300

    Update googleUploader.js

--- src/utils/googleUploader.js
+++ src/utils/googleUploader.js
@@ -1,9 +1,25 @@
 import { google } from 'googleapis';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: 'polyhub-a0552ac28054.json', // файл із Google Cloud Console
+  credentials: {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN,
+  },
   scopes: ['https://www.googleapis.com/auth/drive'],
 });
+
 
 const driveService = google.drive({ version: 'v3', auth });
 
