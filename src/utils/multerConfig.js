9612ce9df3d5f0ea604c5afee808dd77d32eb391
bronchitis4 <79788949+bronchitis4@users.noreply.github.com>
2025-06-05 01:50:30 +0300

    Update multerConfig.js

--- src/utils/multerConfig.js
+++ src/utils/multerConfig.js
@@ -30,15 +30,15 @@ const storage = multer.diskStorage({
   }
 });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '../uploads/');
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, uniqueSuffix + path.extname(file.originalname));
-    }
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, '../uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//       cb(null, uniqueSuffix + path.extname(file.originalname));
+//     }
+// });
 
 
 
